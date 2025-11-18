b2560f3ce8a16d63f7d4ffe71da78deff9f49cef
syedmehedi34 <syedmehedi34@gmail.com>
2025-11-19 03:39:52 +0600

    added logo

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,13 @@
-export const SENDER_NAME = process.env.SENDER_NAME || "support";
-export const SENDER_EMAIL = process.env.SENDER_EMAIL || "onboarding@resend.dev";
+// export const SENDER_NAME = process.env.SENDER_NAME || "support";
+// export const SENDER_EMAIL = process.env.SENDER_EMAIL || "onboarding@resend.dev";
 
-export const USER_ROLES = ["Admin", "User"];
-export const COLORS = ["Gold", "Green", "Red"];
-export const THEMES = ["Light", "Dark", "System"];
+// export const USER_ROLES = ["Admin", "User"];
+// export const COLORS = ["Gold", "Green", "Red"];
+// export const THEMES = ["Light", "Dark", "System"];
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "QuickMart";
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Shop Smart, Live Better";
+
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "The ultimate online marketplace for all your shopping needs.";
