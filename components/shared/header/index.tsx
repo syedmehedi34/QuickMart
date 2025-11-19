00c036b9eda2fc62d33e333fe110a482f4185377
syedmehedi34 <syedmehedi34@gmail.com>
2025-11-19 18:30:23 +0600

    carousel page implemented

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -34,7 +34,7 @@ const Header = () => {
       <div className="flex items-center px-3 mb-px  bg-gray-800">
         <Button
           variant="ghost"
-          className="header-button flex items-center gap-1 text-base [&_svg]:size-6"
+          className="dark header-button flex items-center gap-1 text-base [&_svg]:size-6"
         >
           <MenuIcon />
           All
--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,18 +1,19 @@
-import { ShoppingCartIcon, UserIcon } from "lucide-react";
+import { ShoppingCartIcon } from "lucide-react";
 import Link from "next/link";
 
 const Menu = () => {
   return (
     <div className="flex justify-end">
       <nav className="md:flex gap-3 hidden w-full">
-        <Link href="/cart" className="header-button">
-          <UserIcon className="h-8 w-8" />
-          <span className="font-bold">Sign In</span>
+        <Link href="/signin" className="flex items-center header-button">
+          Hello, Sign In
         </Link>
 
         <Link href="/cart" className="header-button">
-          <ShoppingCartIcon className="h-8 w-8" />
-          <span className="font-bold">Cart</span>
+          <div className="flex items-end">
+            <ShoppingCartIcon className="h-8 w-8" />
+            Cart
+          </div>
         </Link>
       </nav>
     </div>
