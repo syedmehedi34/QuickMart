0c04a3aa2120a1dd6cc3b63f6adccfde8b304578
syedmehedi34 <syedmehedi34@gmail.com>
2025-11-20 12:33:48 +0600

    updated product-card

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -16,6 +16,86 @@ import ProductPrice from "./product-price";
 import ImageHover from "./image-hover";
 import AddToCart from "./add-to-cart";
 
+// --------------------
+// Sub Components (FIX)
+// --------------------
+const ProductImage = ({ product }: { product: IProduct }) => (
+  <Link href={`/product/${product.slug}`}>
+    <div className="relative h-52">
+      {product.images.length > 1 ? (
+        <ImageHover
+          src={product.images[0]}
+          hoverSrc={product.images[1]}
+          alt={product.name}
+        />
+      ) : (
+        <div className="relative h-52">
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            fill
+            sizes="80vw"
+            className="object-contain"
+          />
+        </div>
+      )}
+    </div>
+  </Link>
+);
+
+const ProductDetails = ({ product }: { product: IProduct }) => (
+  <div className="flex-1 space-y-2">
+    <p className="font-bold">{product.brand}</p>
+    <Link
+      href={`/product/${product.slug}`}
+      className="overflow-hidden text-ellipsis"
+      style={{
+        display: "-webkit-box",
+        WebkitLineClamp: 2,
+        WebkitBoxOrient: "vertical",
+      }}
+    >
+      {product.name}
+    </Link>
+
+    <div className="flex gap-2 justify-center">
+      <Rating rating={product.avgRating} />
+      <span>({formatNumber(product.numReviews)})</span>
+    </div>
+
+    <ProductPrice
+      isDeal={product.tags.includes("todays-deal")}
+      price={product.price}
+      listPrice={product.listPrice}
+      forListing
+    />
+  </div>
+);
+
+const AddButton = ({ product }: { product: IProduct }) => (
+  <div className="w-full text-center">
+    <AddToCart
+      minimal
+      item={{
+        clientId: generateId(),
+        product: product._id,
+        size: product.sizes[0],
+        color: product.colors[0],
+        countInStock: product.countInStock,
+        name: product.name,
+        slug: product.slug,
+        category: product.category,
+        price: round2(product.price),
+        quantity: 1,
+        image: product.images[0],
+      }}
+    />
+  </div>
+);
+
+// --------------------
+// Main Component
+// --------------------
 const ProductCard = ({
   product,
   hideBorder = false,
@@ -27,101 +107,34 @@ const ProductCard = ({
   hideBorder?: boolean;
   hideAddToCart?: boolean;
 }) => {
-  const ProductImage = () => (
-    <Link href={`/product/${product.slug}`}>
-      <div className="relative h-52">
-        {product.images.length > 1 ? (
-          <ImageHover
-            src={product.images[0]}
-            hoverSrc={product.images[1]}
-            alt={product.name}
-          />
-        ) : (
-          <div className="relative h-52">
-            <Image
-              src={product.images[0]}
-              alt={product.name}
-              fill
-              sizes="80vw"
-              className="object-contain"
-            />
-          </div>
-        )}
-      </div>
-    </Link>
-  );
-  const ProductDetails = () => (
-    <div className="flex-1 space-y-2">
-      <p className="font-bold">{product.brand}</p>
-      <Link
-        href={`/product/${product.slug}`}
-        className="overflow-hidden text-ellipsis"
-        style={{
-          display: "-webkit-box",
-          WebkitLineClamp: 2,
-          WebkitBoxOrient: "vertical",
-        }}
-      >
-        {product.name}
-      </Link>
-      <div className="flex gap-2 justify-center">
-        <Rating rating={product.avgRating} />
-        <span>({formatNumber(product.numReviews)})</span>
-      </div>
-
-      <ProductPrice
-        isDeal={product.tags.includes("todays-deal")}
-        price={product.price}
-        listPrice={product.listPrice}
-        forListing
-      />
-    </div>
-  );
-  const AddButton = () => (
-    <div className="w-full text-center">
-      <AddToCart
-        minimal
-        item={{
-          clientId: generateId(),
-          product: product._id,
-          size: product.sizes[0],
-          color: product.colors[0],
-          countInStock: product.countInStock,
-          name: product.name,
-          slug: product.slug,
-          category: product.category,
-          price: round2(product.price),
-          quantity: 1,
-          image: product.images[0],
-        }}
-      />
-    </div>
-  );
-
   return hideBorder ? (
     <div className="flex flex-col">
-      <ProductImage />
+      <ProductImage product={product} />
+
       {!hideDetails && (
         <>
           <div className="p-3 flex-1 text-center">
-            <ProductDetails />
+            <ProductDetails product={product} />
           </div>
-          {!hideAddToCart && <AddButton />}
+
+          {!hideAddToCart && <AddButton product={product} />}
         </>
       )}
     </div>
   ) : (
-    <Card className="flex flex-col  ">
+    <Card className="flex flex-col">
       <CardHeader className="p-3">
-        <ProductImage />
+        <ProductImage product={product} />
       </CardHeader>
+
       {!hideDetails && (
         <>
-          <CardContent className="p-3 flex-1  text-center">
-            <ProductDetails />
+          <CardContent className="p-3 flex-1 text-center">
+            <ProductDetails product={product} />
           </CardContent>
+
           <CardFooter className="p-3">
-            {!hideAddToCart && <AddButton />}
+            {!hideAddToCart && <AddButton product={product} />}
           </CardFooter>
         </>
       )}
