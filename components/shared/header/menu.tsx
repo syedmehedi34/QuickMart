ecb7af5ed75c32799e48de12357700401398fad2
syedmehedi34 <syedmehedi34@gmail.com>
2025-11-21 01:30:08 +0600

    add-to-card button added

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,5 +1,5 @@
-import { ShoppingCartIcon } from "lucide-react";
 import Link from "next/link";
+import CartButton from "./cart-button";
 
 const Menu = () => {
   return (
@@ -9,12 +9,7 @@ const Menu = () => {
           Hello, Sign In
         </Link>
 
-        <Link href="/cart" className="header-button">
-          <div className="flex items-end">
-            <ShoppingCartIcon className="h-8 w-8" />
-            Cart
-          </div>
-        </Link>
+        <CartButton />
       </nav>
     </div>
   );
--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,20 +1,25 @@
 /* eslint-disable react-hooks/static-components */
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import { IProduct } from "@/lib/db/models/product.model";
 
 import Rating from "./rating";
-import { formatNumber } from "@/lib/utils";
+import { formatNumber, generateId, round2 } from "@/lib/utils";
 import ProductPrice from "./product-price";
 import ImageHover from "./image-hover";
+import AddToCart from "./add-to-cart";
 
 const ProductCard = ({
   product,
   hideBorder = false,
   hideDetails = false,
+  hideAddToCart = false,
 }: {
   product: IProduct;
   hideDetails?: boolean;
@@ -72,6 +77,27 @@ const ProductCard = ({
     </div>
   );
 
+  const AddButton = () => (
+    <div className="w-full text-center">
+      <AddToCart
+        minimal
+        item={{
+          clientId: generateId(),
+          product: product._id,
+          size: product.sizes[0],
+          color: product.colors[0],
+          countInStock: product.countInStock,
+          name: product.name,
+          slug: product.slug,
+          category: product.category,
+          price: round2(product.price),
+          quantity: 1,
+          image: product.images[0],
+        }}
+      />
+    </div>
+  );
+
   return hideBorder ? (
     <div className="flex flex-col">
       <ProductImage />
@@ -80,6 +106,7 @@ const ProductCard = ({
           <div className="p-3 flex-1 text-center">
             <ProductDetails />
           </div>
+          {!hideAddToCart && <AddButton />}
         </>
       )}
     </div>
@@ -93,6 +120,9 @@ const ProductCard = ({
           <CardContent className="p-3 flex-1  text-center">
             <ProductDetails />
           </CardContent>
+          <CardFooter className="p-3">
+            {!hideAddToCart && <AddButton />}
+          </CardFooter>
         </>
       )}
     </Card>
