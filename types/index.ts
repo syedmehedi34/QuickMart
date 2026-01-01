e3e52cbdba0dfcc927013936236dc35d60a96f54
syedmehedi34 <syedmehedi34@gmail.com>
2026-01-02 03:13:07 +0600

    category sidebar drawer created

--- types/index.ts
+++ types/index.ts
@@ -3,18 +3,33 @@ import {
   OrderInputSchema,
   OrderItemSchema,
   ProductInputSchema,
+  ReviewInputSchema,
   ShippingAddressSchema,
   UserInputSchema,
+  UserNameSchema,
   UserSignInSchema,
   UserSignUpSchema,
 } from "@/lib/validator";
 import { z } from "zod";
 
+export type IReviewInput = z.infer<typeof ReviewInputSchema>;
+export type IReviewDetails = IReviewInput & {
+  _id: string;
+  createdAt: string;
+  user: {
+    name: string;
+  };
+};
 export type IProductInput = z.infer<typeof ProductInputSchema>;
 
 export type Data = {
   users: IUserInput[];
   products: IProductInput[];
+  reviews: {
+    title: string;
+    rating: number;
+    comment: string;
+  }[];
   headerMenus: {
     name: string;
     href: string;
@@ -37,3 +52,4 @@ export type ShippingAddress = z.infer<typeof ShippingAddressSchema>;
 export type IUserInput = z.infer<typeof UserInputSchema>;
 export type IUserSignIn = z.infer<typeof UserSignInSchema>;
 export type IUserSignUp = z.infer<typeof UserSignUpSchema>;
+export type IUserName = z.infer<typeof UserNameSchema>;
